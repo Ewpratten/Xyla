56dd35106a83ad19f2972a9f7f78180e119e8a93
Evan Pratten <ewpratten@gmail.com>
2020-01-18 22:30:31 -0500

    base project

---
+++ desktop/src/ca/retrylife/xyla/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ca.retrylife.xyla.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ca.retrylife.xyla.Xyla;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Xyla(), config);
+	}
+}
