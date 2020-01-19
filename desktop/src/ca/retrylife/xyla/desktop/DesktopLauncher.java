ff87b49e9d0b6b2719d119b91887f4f433ba6b90
Evan Pratten <ewpratten@gmail.com>
2020-01-18 22:36:18 -0500

    cfg

--- desktop/src/ca/retrylife/xyla/desktop/DesktopLauncher.java
+++ desktop/src/ca/retrylife/xyla/desktop/DesktopLauncher.java
@@ -5,8 +5,15 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import ca.retrylife.xyla.Xyla;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main(String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		// Set application config
+		config.title = "Xyla";
+		config.width = 1480;
+		config.height = 800;
+
+		// Start application
 		new LwjglApplication(new Xyla(), config);
 	}
 }
